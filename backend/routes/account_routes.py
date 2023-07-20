ff7d54430730d937876f96f727f4128142c68156
deano <dean0coin@protonmail.com>
2023-07-20 14:04:14 -0400

    Update email endpoint
    Current and new email verification codes required to change email

--- backend/routes/account_routes.py
+++ backend/routes/account_routes.py
@@ -19,7 +19,7 @@ smtp_client.connect()
 router = APIRouter()
 
 @router.post("/items/update/reqpassword")
-async def request_update_password(request: Request, is_auth: bool = Depends(auth_dependency)):
+async def request_password_update(request: Request, is_auth: bool = Depends(auth_dependency)):
     id = from_jwt(str(request.cookies.get("authenticate")))
     user = json.loads(db.user_manager.get_user_data_by_id(id))
     
@@ -28,6 +28,27 @@ async def request_update_password(request: Request, is_auth: bool = Depends(auth
     smtp_client.send_verification_code(user["email"], v_code)
     
     return Response(status_code=200)
+    
+    
+@router.post("/items/update/reqemail") # JSON EX: {"email":"my_new_email@example.com"}
+async def request_email_update(request: Request, is_auth: bool = Depends(auth_dependency)):
+    try: email_data = await request.json()
+    except json.decoder.JSONDecodeError: return Response(status_code=400)
+
+    id = from_jwt(str(request.cookies.get("authenticate")))
+    user = json.loads(db.user_manager.get_user_data_by_id(id))
+    new_email = email_data["email"]
+    
+    cur_code = str(randint_n(16))
+    new_code = str(randint_n(16))
+    
+    db.user_manager.update_column(id, "verification_code", f"{cur_code}#{new_code}")
+    smtp_client.send_verification_code(user["email"], cur_code)
+    smtp_client.send_verification_code(new_email, new_code)
+    
+    return Response(status_code=200)
+    
+    
     
     
 @router.post("/items/update/password") # JSON EX: {"password":"my_new_password", "code":"123456"}
@@ -40,8 +61,9 @@ async def update_password(request: Request, is_auth: bool = Depends(auth_depende
     
     new_password = hash_password(pw_data["password"])
     in_code = pw_data["code"]
+    user_ver_code = str(user["verification_code"]).split("#")[0]
     
-    if str(in_code) == str(user["verification_code"]):
+    if str(in_code) == user_ver_code:
         db.user_manager.update_column(id, "password", new_password)
         db.user_manager.update_column(id, "verification_code", "")
         return Response(status_code=204) # Valid code - password updated
@@ -49,9 +71,24 @@ async def update_password(request: Request, is_auth: bool = Depends(auth_depende
     return Response(status_code=400) # Invalid code
     
     
+@router.post("/items/update/email") # JSON EX: {"email":"my_new_email@example.com", "cur_code":"123456", "new_code":"654321"}
+async def update_email(request: Request, is_auth: bool = Depends(auth_dependency)):
+    try: email_data = await request.json()
+    except json.decoder.JSONDecodeError: return Response(status_code=400)
     
+    id = from_jwt(str(request.cookies.get("authenticate")))
+    user = json.loads(db.user_manager.get_user_data_by_id(id))
     
+    new_email = email_data["email"]
+    cur_code = email_data["cur_code"]
+    new_code = email_data["new_code"]
     
+    user_ver_code1 = str(user["verification_code"]).split("#")[0] # current email verification code
+    user_ver_code2 = str(user["verification_code"]).split("#")[1] # new email verification code
     
-    
-    
+    if str(cur_code) == user_ver_code1 and str(new_code) == user_ver_code2:
+        db.user_manager.update_column(id, "email", new_email)
+        db.user_manager.update_column(id, "verification_code", "")
+        return Response(status_code=204) # Valid code - password updated
+        
+    return Response(status_code=400) # One of the codes are invalid
